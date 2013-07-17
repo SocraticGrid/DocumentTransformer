c974afe50253f8b488f0e91c07512abb6b04742e
Jerry Goodnough <ferret@stomrwoods.com>
2013-07-17 11:29:57 -0700

    Add Diagnositics

--- src/main/java/org/socraticgrid/documenttransformer/Transformer.java
+++ src/main/java/org/socraticgrid/documenttransformer/Transformer.java
@@ -1,80 +1,126 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+/*-
+ *
+ * *************************************************************************************************************
+ *  Copyright (C) 2013 by Cognitive Medical Systems, Inc
+ *  (http://www.cognitivemedciine.com) * * Licensed under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except in compliance *
+ *  with the License. You may obtain a copy of the License at * *
+ *  http://www.apache.org/licenses/LICENSE-2.0 * * Unless required by applicable
+ *  law or agreed to in writing, software distributed under the License is *
+ *  distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied. * See the License for the specific language
+ *  governing permissions and limitations under the License. *
+ * *************************************************************************************************************
+ *
+ * *************************************************************************************************************
+ *  Socratic Grid contains components to which third party terms apply. To comply
+ *  with these terms, the following * notice is provided: * * TERMS AND
+ *  CONDITIONS FOR USE, REPRODUCTION, AND DISTRIBUTION * Copyright (c) 2008,
+ *  Nationwide Health Information Network (NHIN) Connect. All rights reserved. *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that * the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright notice, this
+ *  list of conditions and the *     following disclaimer. * - Redistributions in
+ *  binary form must reproduce the above copyright notice, this list of
+ *  conditions and the *     following disclaimer in the documentation and/or
+ *  other materials provided with the distribution. * - Neither the name of the
+ *  NHIN Connect Project nor the names of its contributors may be used to endorse
+ *  or *     promote products derived from this software without specific prior
+ *  written permission. * * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS
+ *  AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED * WARRANTIES, INCLUDING,
+ *  BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *  A * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+ *  OR CONTRIBUTORS BE LIABLE FOR * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ *  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, *
+ *  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ *  OR BUSINESS INTERRUPTION HOWEVER * CAUSED AND ON ANY THEORY OF LIABILITY,
+ *  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ *  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, * EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. * * END OF TERMS AND CONDITIONS *
+ * *************************************************************************************************************/
 package org.socraticgrid.documenttransformer;
 
 import org.socraticgrid.documenttransformer.interfaces.SingleSourcePipeline;
+
 import java.io.InputStream;
-import java.lang.String;
+
 import java.util.HashMap;
 import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 /**
+ * DOCUMENT ME!
  *
- * @author Jerry Goodnough
+ * @author  Jerry Goodnough
  */
 public class Transformer
 {
-  
-    private HashMap<String, SingleSourcePipeline> transformPipeline; 
-    
-    //Factory
-    //Initialization
-    //Transfomation
+    private static final Logger logger = Logger.getLogger(Transformer.class
+            .getName());
+    private HashMap<String, SingleSourcePipeline> transformPipeline;
 
-    //static
-    //{
-    //    System.setProperty("javax.xml.transform.TransformerFactory", "net.sf.saxon.TransformerFactoryImpl");
-    //}
-
-    public void setTransformPipeline(HashMap<String, SingleSourcePipeline> transformPipeline)
+    // Factory Initialization Transfomation static {
+    // System.setProperty("javax.xml.transform.TransformerFactory",
+    // "net.sf.saxon.TransformerFactoryImpl"); }
+    public void setTransformPipeline(
+        HashMap<String, SingleSourcePipeline> transformPipeline)
     {
-        this.transformPipeline=transformPipeline;
+        this.transformPipeline = transformPipeline;
     }
-    
+
     public String transform(String pipeline, InputStream inStr)
     {
         String out = null;
+
         if (transformPipeline.containsKey(pipeline))
         {
             out = transformPipeline.get(pipeline).transform(inStr);
         }
-   
+
         return out;
     }
-    
+
     public String transform(String pipeline, InputStream inStr, Properties props)
     {
         String out = null;
+
         if (transformPipeline.containsKey(pipeline))
         {
-            out = transformPipeline.get(pipeline).transform(inStr,props);
+            out = transformPipeline.get(pipeline).transform(inStr, props);
         }
-   
+
         return out;
     }
-    
+
     public InputStream transformAsStream(String pipeline, InputStream inStr)
     {
         InputStream out = null;
+
         if (transformPipeline.containsKey(pipeline))
         {
             out = transformPipeline.get(pipeline).transformAsInputStream(inStr);
         }
-   
+
         return out;
     }
-    
-    public InputStream transformAsStream(String pipeline, InputStream inStr, Properties props)
+
+    public InputStream transformAsStream(String pipeline, InputStream inStr,
+        Properties props)
     {
         InputStream out = null;
+
         if (transformPipeline.containsKey(pipeline))
         {
-            out = transformPipeline.get(pipeline).transformAsInputStream(inStr,props);
+            out = transformPipeline.get(pipeline).transformAsInputStream(inStr,
+                    props);
         }
-   
+        else
+        {
+            logger.log(Level.WARNING, "{0} not found in transformer", pipeline);
+        }
+
         return out;
     }
-    
 }
